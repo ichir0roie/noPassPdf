a8a74f1bd045951ad7b7a5dd6a6eeb75d78d7438
IchiroT <ichir0roie@gmail.com>
2020-12-29 12:55:28 +0900

    format code

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import os
 import PyPDF2
 
-
 import glob
 
+# todo learn gitIgnore
 
-filepaths=glob.glob("Data/*.pdf")
+filePaths = glob.glob("Data/*.pdf")
 
-password=""
 
-with open("Password",mode="r",encoding="utf-8")as f:
-    password=f.read().replace("\n","")
+with open("Password", mode="r", encoding="utf-8")as f:
+    password = f.read().replace("\n", "")
 
-for i in filepaths:
-    pdf=PyPDF2.PdfFileReader(i)
+for i in filePaths:
+    pdf = PyPDF2.PdfFileReader(i)
     pdf.decrypt(password)
-    dst_pdf=PyPDF2.PdfFileWriter()
+    dst_pdf = PyPDF2.PdfFileWriter()
     dst_pdf.cloneReaderDocumentRoot(pdf)
 
-    with open(i,mode="wb")as f:
+    with open(i, mode="wb")as f:
         dst_pdf.write(f)
